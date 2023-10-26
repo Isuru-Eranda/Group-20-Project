d8c02167f1b968c66f37d6bb389899bf0aeacf0f
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-10-26 14:18:07 +0530

    Updated Error handling
    10/26/2023

--- Contact.php
+++ Contact.php
@@ -4,6 +4,17 @@
 	$heading = $_POST['subject'];
 	$message = $_POST['message'];
 
+    error_reporting(E_ALL);
+    ini_set("display_errors", 0);
+
+    function customErrorHandler($errno, $errstr, $errfile, $errline)
+    {
+        $message = "Error: [$errno] $errstr - $errfile:$errline";
+        error_log($message . PHP_EOL, 3, "error_log.txt");
+    }
+
+    set_error_handler("customErrorHandler");
+
 	if(!empty($email) && !empty($message)) {
 		if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			$receiver = "garukavassalaarachchi@gmail.com";
